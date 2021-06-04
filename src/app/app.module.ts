ee7873c381b1eccfbef509a93bae34e6acdc5727
Sarawut <srwkk.8@gmail.com>
2021-06-04 12:18:01 +0700

    Homework-recipe: set up project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductitemComponent } from './productitem/productitem.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductitemComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/productitem/productitem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productitem',
+  templateUrl: './productitem.component.html',
+  styleUrls: ['./productitem.component.css']
+})
+export class ProductitemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
